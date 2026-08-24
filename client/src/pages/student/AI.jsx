1b29ca73c09b44a207a42db1e8bd829d03c8300b
Kavinrajsaravanakumar <145635656+Kavinrajsaravanakumar@users.noreply.github.com>
2026-08-24 14:23:52 +0530

    Update API_URL to use environment variable

--- client/src/pages/student/AI.jsx
+++ client/src/pages/student/AI.jsx
@@ -21,7 +21,7 @@ import {
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL =  import.meta.env.VITE_API_URL;
 
 const AI = () => {
   const [chatInput, setChatInput] = useState("");
